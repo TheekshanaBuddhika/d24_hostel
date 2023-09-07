c04a29c993a422b6a8b1be9364ea58324e2c9946
Tech-Sergent <124680474+Tech-Sergent@users.noreply.github.com>
2023-09-08 00:46:34 +0530

    entity add

---
+++ src/main/java/lk/ijse/entity/Room.java
@@ -0,0 +1,35 @@
+package lk.ijse.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "room")
+public class Room {
+    @Id
+    @Column(name = "room_id",length = 50)
+    private String id;
+    @Column(name = "room_type")
+    private String type;
+    @Column(name = "room_key_money")
+    private String key_money;
+    @Column(name = "room_qty")
+    private int qty;
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER, mappedBy =  "room")
+    private List<Reservation> reservations = new ArrayList<>();
+
+    public Room(String id, String type, String keyMoney, int qty) {
+        this.id=id;
+        this.type=type;
+        this.key_money=keyMoney;
+        this.qty=qty;
+    }
+}
---
+++ src/main/java/lk/ijse/entity/Student.java
@@ -0,0 +1,43 @@
+package lk.ijse.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+
+@Entity
+@Table(name = "student")
+public class Student {
+    @Id
+    @Column(name = "student_id",length = 50)
+    private String id;
+    @Column(name = "student_name")
+    private String name;
+    @Column(name = "address")
+    private String address;
+    @Column(name = "student_contact")
+    private String contact;
+    @Column(name = "student_dob")
+    private Date dob;
+    @Column(name = "student_gender")
+    private String gender;
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER, mappedBy =  "student")
+    private List<Reservation> reservations = new ArrayList<>();
+
+    public Student(String sId, String name, String address, String contact, Date dob, String gen) {
+        this.name=name;
+        this.id=sId;
+        this.address=address;
+        this.contact=contact;
+        this.dob=dob;
+        this.gender=gen;
+    }
+}
